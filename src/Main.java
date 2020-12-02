b8f16d120b6c84665ea477e5936510e8a79b3cad
Andreas Wögerbauer <andreas.woegerbauer@htl-wels.at>
2020-12-02 12:29:13 +0100

    create target folder if not exists

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,17 @@ import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 public class Main {
     public static void main(String[] args) {
 
-        String dir = "C:\\Users\\andre\\Downloads\\OneDrive_2_10.11.2020";
+        String dir = "C:\\Users\\andre\\Downloads\\OneDrive";
         String target = "C:\\Users\\andre\\Downloads\\Target";
-        String sd = "Aufgabe 3";
+        String sd = "Aufgabe 5";
 
         try {
+            //create target directory if not exists
+            File directory = new File(target);
+            if (! directory.exists()){
+                directory.mkdir();
+            }
+
             //Files.find(Paths.get(dir), 999, (p, bfa) -> bfa.isRegularFile()).forEach(System.out::println);
             Stream<Path> r = Files.find(Paths.get(dir), 999, (p, bfa) -> {
                 return bfa.isDirectory();
@@ -42,43 +48,6 @@ public class Main {
         }
 
         System.out.println("The end!");
-    }
-
-    static void copyFolder(File src, File dest) {
-        // checks
-        if(src==null || dest==null)
-            return;
-        if(!src.isDirectory())
-            return;
-        if(dest.exists()){
-            if(!dest.isDirectory()){
-                //System.out.println("destination not a folder " + dest);
-                return;
-            }
-        } else {
-            dest.mkdir();
-        }
-
-        File[] files = src.listFiles();
-        if(files==null || files.length==0)
-            return;
-
-        for(File file: files){
-            File fileDest = new File(dest, file.getName());
-            //System.out.println(fileDest.getAbsolutePath());
-            if(file.isDirectory()){
-                copyFolder(file, fileDest);
-            }else{
-                if(fileDest.exists())
-                    continue;
-
-                try {
-                    Files.copy(file.toPath(), fileDest.toPath());
-                } catch (IOException e) {
-                    //e.printStackTrace();
-                }
-            }
-        }
     }
 
     public static void copyFolder(Path src, Path dest) throws IOException {
---
+++ src/deploy.bat
@@ -0,0 +1,3 @@
+copy *.java C:\Temp\HTL\Projekte\Deploy\src\
+rmdir /S /Q C:\Temp\HTL\Projekte\Deploy\out\
+PAUSE
